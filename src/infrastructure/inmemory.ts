399840e4d4437dfdc122310b212a743e639bdef0
Ryn54 <ryenth54@gmail.com>
2023-06-22 16:06:26 +0200

    inmemory repo implem

--- src/infrastructure/inmemory.ts
+++ src/infrastructure/inmemory.ts
@@ -2,19 +2,31 @@ import { Asset } from '../domain/asset';
 
 import { AssetRepository } from './repository';
 
-// TODO
 export class InMemoryRepository implements AssetRepository {
   private assets: Asset[] = [];
 
   getAsset(assetHash: string): Promise<Asset> {
-    throw new Error('Method not implemented.');
+    const asset = this.assets.find((asset: Asset) => {
+      return asset.assetHash === assetHash;
+    });
+
+    if (!asset) throw new Error('not found');
+
+    return Promise.resolve(asset);
   }
 
   createAsset(asset: Asset): Promise<void> {
-    throw new Error('Method not implemented.');
+    this.assets.push(asset);
+    return Promise.resolve();
   }
 
-  setAssetEnable(assetHash: string): Promise<void> {
-    throw new Error('Method not implemented.');
+  async setAssetEnable(assetHash: string): Promise<void> {
+    const asset = await this.getAsset(assetHash);
+    if (asset.isEnable === true) return;
+
+    // si isEnable est false
+    asset.isEnable = true;
+    this.assets.filter((asset: Asset) => asset.assetHash !== assetHash);
+    this.assets.push(asset);
   }
 }
