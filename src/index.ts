66a26a8f666e1e1b3eb8a8485be78841b63a965c
Ryn54 <ryenth54@gmail.com>
2023-07-03 15:12:37 +0200

    add roboto folder in code

--- src/index.ts
+++ src/index.ts
@@ -3,43 +3,43 @@ import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
 
-// import { EsploraChainSource } from './application/chainsource';
-// import { Updater } from './application/updater';
-// import { AssetController } from './controllers/asset';
-// import { connect } from './infrastructure/mongo';
+import { EsploraChainSource } from './application/chainsource';
+import { Updater } from './application/updater';
+import { AssetController } from './controllers/asset';
+import { connect } from './infrastructure/mongo';
 import { logger } from './middlewares/logger';
-// import { makeAssetRouter } from './routes/asset';
+import { makeAssetRouter } from './routes/asset';
 
 dotenv.config();
 
 async function run() {
-  // if (!process.env.DATABASE_URL) {
-  //   throw new Error('DATABASE_URL is not defined');
-  // }
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined');
+  }
 
-  // if (!process.env.ESPLORA_URL) {
-  //   throw new Error('ESPLORA_URL is not defined');
-  // }
+  if (!process.env.ESPLORA_URL) {
+    throw new Error('ESPLORA_URL is not defined');
+  }
 
-  // const [assetRepo, txsRepo] = await connect(process.env.DATABASE_URL);
+  const [assetRepo, txsRepo] = await connect(process.env.DATABASE_URL);
 
   console.log('starting the server...');
   const app = express();
   app.listen(5000);
 
-  // const controller = new AssetController(assetRepo, txsRepo);
+  const controller = new AssetController(assetRepo, txsRepo);
 
-  // // const chainSource = new EsploraChainSource(process.env.ESPLORA_URL);
-  // const updater = new Updater(chainSource, txsRepo);
+  const chainSource = new EsploraChainSource(process.env.ESPLORA_URL);
+  const updater = new Updater(chainSource, txsRepo);
 
-  // const assetRouter = makeAssetRouter(controller, updater);
+  const assetRouter = makeAssetRouter(controller, updater);
 
   app.use(logger);
   app.use(express.json());
-  // app.use('/api/asset', assetRouter);
+  app.use('/api/asset', assetRouter);
 
-  // serve static views/index.html file via GET /supply/html
-  app.get('/supply/html', (_, res) => {
+  //serve static views/index.html file via GET /supply/html
+  app.get('/supply/graph', (_, res) => {
     // TODO: implement this
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
